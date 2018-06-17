8dae67ea280148056d735468101defbff0c06258
henghao lee <lhh1227@163.com>
2018-06-17 16:48:24 +0800

    Initial commit

---
+++ arm_control/arm_control.py
@@ -0,0 +1,55 @@
+#import math
+#pi = 3.1415926535
+#a=float(10.35)
+#b=float(12.65)
+#x=float(input("x: "))
+#y=float(input("y: "))
+#z=float(input("z: "))
+#delta=x*x+y*y+z*z
+#A=pi/2 - math.asin(z/math.sqrt(delta)) - math.acos((a*a-b*b+delta)/(2*a*math.sqrt(delta)))
+#B=math.acos((a*a-b*b+delta)/(2*a*math.sqrt(delta))) + math.acos((delta+b*b-a*a)/(2*b*math.sqrt(delta)))
+#if x==0 and y==0:
+#    O = 0
+#elif y==0:
+#    O = pi/2
+#elif x==0:
+#    O = 0
+#else:
+#    O=math.atan(x/y)
+#angle_A = 180*A/pi
+#angle_B = 180*B/pi
+#angle_O = 180*O/pi
+#print(angle_A)
+#print(angle_B)
+#print(angle_O)
+#print((-1*angle_A+90)/0.13792+850)
+#print((angle_B+90)/0.10039+550)
+#print((angle_O+90)/0.13792+850)
+
+###import math
+###pi = 3.1415926535
+###a=float(10.35)
+###b=float(12.65)
+###x=float(input("x: "))
+###y=float(input("y: "))
+###z=float(input("z: "))
+###delta=x*x+y*y+z*z
+###A=pi/2 - math.asin(z/math.sqrt(delta)) - math.acos((a*a-b*b+delta)/(2*a*math.sqrt(delta)))
+###B=math.acos((a*a-b*b+delta)/(2*a*math.sqrt(delta))) + math.acos((delta+b*b-a*a)/(2*b*math.sqrt(delta)))
+###if x==0 and y==0:
+###    O = 0
+###elif y==0:
+###    O = pi/2
+###elif x==0:
+###    O = 0
+###else:
+###    O=math.atan(x/y)
+###angle_A = 180*A/pi
+###angle_B = 180*B/pi
+###angle_O = 180*O/pi
+###print(angle_A)
+###print(angle_B)
+###print(angle_O)
+###print((-1*angle_A+90)/0.13792+850)
+###print((angle_B+90)/0.10039+550)
+###print((angle_O+90)/0.13792+850)
